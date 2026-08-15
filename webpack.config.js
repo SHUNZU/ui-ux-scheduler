44d39a6397ebab805e15ad00322dfb0872f170ed
cs <cs@csdeMac-mini.local>
2026-08-15 22:24:55 +0800

    Add scheduler favicon

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public/index.html")
+      template: path.resolve(__dirname, "public/index.html"),
+      favicon: path.resolve(__dirname, "public/favicon.svg")
     })
   ],
   devServer: {
